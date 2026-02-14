10eeacd1bb18af1fddd12a4a6f3ff0d6fcc0c9ef
kyraelenaflores-hub <kyraelenaflores@gmail.com>
2026-02-14 18:17:29 +0000

    added course and functions

--- super-mario.js
+++ super-mario.js
@@ -1,6 +1,7 @@
 // Player position
 let x = 100;
 let y = 300;
+let speed = 5;
 
 // Jump state
 let jumping = false;
@@ -14,10 +15,13 @@ function draw() {
   background(120, 190, 255); // sky
 
   // ground
+  stroke("black");
   fill(60, 200, 90);
   rect(0, 330, width, 70);
   updateJump();
 
+  drawCourse();
+  updateMovement();
   drawPlayer();
 }
 
@@ -31,11 +35,34 @@ function jump() {
   }
 }
 
-function keyPressed() {
-  if (key === " ") jump();
-
+function updateMovement() {
+  if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) {
+    x -= speed;
+  }
+  if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)) {
+    x += speed;
+  }
+  
+  // Keep player on screen
+  x = constrain(x, 0, width - 40);
 }
 
+function keyPressed() {
+  if (key === " " || key === "ArrowUp" || key === "w") {
+    if (!jumping) {
+      jumping = true;
+      jumpFrame = 0;
+    }
+  }
+}
+
+function moveRight(){
+  x = x + 30;
+}
+
+function moveLeft(){
+  x = x - 30;
+}
 
 // ==================================================
 // 🧠 JUMP LOGIC
@@ -59,6 +86,28 @@ function updateJump() {
 // 🎨 DRAW PLAYER
 // ==================================================
 function drawPlayer() {
-  fill(255, 60, 60);
-  rect(x, y, 40, 40);
+  stroke("black");
+  fill("rgb(139,51,197)");
+  rect(x, y, 30, 40);
+  fill("rgb(214,189,137)");
+  circle(x+15, y-15, 30);
+}
+
+function drawCourse(){
+  stroke("rgb(75,57,32)");
+  fill("rgb(148,93,15)");
+  rect(200, 310, 30, 30);
+  rect(400, 310, 30, 30);
+  stroke("white");
+  fill("white");
+  drawClouds(50, 50, 50);
+  drawClouds(300, 150, 60);
+  drawClouds(470, 105, 50);
+  drawClouds(135, 230, 50);
+}
+
+function drawClouds(x, y, r){
+  circle(x, y, r);
+  circle(x+20, y-20, r-10);
+  circle(x+40, y, r+10);
 }
